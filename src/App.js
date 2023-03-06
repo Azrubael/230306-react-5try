e967f56f99846859922a4eac6725aec030510af8
Azrubael <i0638464000@gmail.com>
2023-03-06 18:22:27 +0200

    Step #02. Props and handlers for Cards

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import { Input } from './input'
 import { Card } from './card'
 
 function App() {
+  const citiesList = ['New York', 'London', 'Tokyo', 'Sydney']
   return (
     <div className="Main">
       <Input />
       <div className="CardList">
-        <Card />
-        <Card />
-        <Card />
+      {
+        citiesList.map((city) => {
+          return <Card key={city} city={city} />
+        })
+      }
       </div>
     </div>
   )
--- src/input/index.js
+++ src/input/index.js
@@ -2,10 +2,24 @@ import React from 'react'
 import '../App.css'
 
 export const Input = () => {
+  const currentDate = new Date().toDateString()
+  const handleOnChange = (e) => {
+    console.log('handleOnChange at', currentDate)
+  }
+  const handleOnClick = (e) => {
+    console.log('handleOnClick at', currentDate)
+  }
+
   return (
     <div className='InputWrap'>
-      <input className='Input' type='text' placeholder='Search...' />
-      <button className='Button'>+</button>
+      <input className='Input'
+        type='text'
+        placeholder='Search...'
+        onChange={handleOnChange}  
+      />
+      <button className='Button'
+        onClick={handleOnClick}
+      >+</button>
     </div>
   )
 }
